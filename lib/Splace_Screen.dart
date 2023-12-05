a3456e56cb319e7606f595afd4e4c802d6cd72d5
tvishabhatt <tvishabhatt1909@gmail.com>
2023-12-05 13:53:38 +0530

    "Bhagavd gita"

--- lib/Splace_Screen.dart
+++ lib/Splace_Screen.dart
@@ -12,14 +12,36 @@ class Splace_Screen extends StatefulWidget{
   }
 
 }
-class Splace_ScreenState extends State<Splace_Screen>{
+class Splace_ScreenState extends State<Splace_Screen> with TickerProviderStateMixin{
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _moveUpAnimation;
+  late AnimationController _controller;
+  late Animation<double> _animation1;
+
   @override
   void initState() {
-
-    // TODO: implement initState
-    Future.delayed(Duration(seconds:5),() =>
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage(),)),);
     super.initState();
+
+     _controller=AnimationController(vsync: this,duration: Duration(seconds: 3));
+     _animation1=CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+     _controller.forward();
+
+
+
+    _animationController=AnimationController(vsync: this,duration: Duration(seconds: 3));
+    _fadeAnimation=Tween<double>(begin: 0,end: 1).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+    _moveUpAnimation = Tween<double>(begin: 0, end: -50).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _animationController.forward();
+
+    Future.delayed(Duration(seconds:5),() =>
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage(),)),);
+
   }
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -30,20 +52,39 @@ class Splace_ScreenState extends State<Splace_Screen>{
             body: Column(mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Center(
-                  child: SizedBox(height: 200,width: 200,
-                    child: ClipRRect
-                      (borderRadius: BorderRadius.circular(20),
-                        child: Image(image: AssetImage("assets/Bhagvad gita image.jpg"),height: double.infinity,width: double.infinity,fit: BoxFit.cover,)),
+                  child: ScaleTransition(
+
+                    scale: _animation1,
+                    child: SizedBox(height: 200,width: 200,
+                      child: ClipRRect
+                        (borderRadius: BorderRadius.circular(20),
+                          child: Image(image: AssetImage("assets/Bhagvad gita image.jpg"),height: double.infinity,width: double.infinity,fit: BoxFit.cover,)),
+                    ),
                   ),
                 ),
-                SizedBox(height: 30,),
-                Center(child: Text("श्रीमद्भगवद्गीता",style: TextStyle(color:themechanger.isDark?Colors.white:Colors.black,fontSize: 22,fontWeight: FontWeight.bold),))
+                SizedBox(height: 70,),
+                Center(
+                  child: AnimatedBuilder(
+                    animation: _animationController,
+                    builder: (context, child) {
+                      return Opacity(
+                          opacity: _fadeAnimation.value,
+                      child: Transform.translate(
+                        offset: Offset(0,_moveUpAnimation.value),
+                      child:Text("श्रीमद्भगवद्गीता",
+                        style: TextStyle(color:themechanger.isDark?Colors.white:Colors.black,fontSize: 22,fontWeight: FontWeight.bold),),),);
+                    },
 
-              ],
+                  ))],
             ),
           );
         }
     );
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
 }
